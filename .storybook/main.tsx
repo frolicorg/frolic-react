331129aa5d100f25befd11f0407f5aad94ed70cd
Arihant Parsoya <parsoyaarihant@gmail.com>
2023-08-06 11:14:29 +0530

    migrated to storybook 7

--- .storybook/main.tsx
+++ .storybook/main.tsx
@@ -1,5 +1,4 @@
 var path = require('path');
-
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", {
@@ -8,27 +7,26 @@ module.exports = {
       cssLoaderOptions: {
         // When you have splitted your css over multiple files
         // and use @import('./other-styles.css')
-        importLoaders: 1,
+        importLoaders: 1
       },
       postcssLoaderOptions: {
         // When using postCSS 8
-        implementation: require('postcss'),
-      },
-    },
+        implementation: require('postcss')
+      }
+    }
   }],
-  "framework": "@storybook/react",
-  core: {
-    builder: "webpack5"
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
   features: {
-    previewMdx2: true,
+    previewMdx2: true
   },
-  webpackFinal: async (config) => {
-    config.resolve.modules = [
-      ...(config.resolve.modules || []),
-      path.resolve(__dirname, "../src"),
-    ];
-
+  webpackFinal: async config => {
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, "../src")];
     return config;
   },
+  docs: {
+    autodocs: true
+  }
 };
